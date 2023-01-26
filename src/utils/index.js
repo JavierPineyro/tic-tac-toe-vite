25074953e75d246059e2b46b4e80275ecc5e923c
JavierPineyro <javidunam22@gmail.com>
2023-01-26 09:41:01 -0300

    Lint

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,12 @@
 import { INDEX_WINNER_COMBINATIONS } from '../constants'
 
-
 export const checkNewWinner = (game) => {
-  for (let combination of INDEX_WINNER_COMBINATIONS) {
-
+  for (const combination of INDEX_WINNER_COMBINATIONS) {
     const [valueA, valueB, valueC] = combination
 
-    const isWinner = game[valueA] === game[valueB]
-      && game[valueB] === game[valueC]
-      && game[valueA] !== null
+    const isWinner = game[valueA] === game[valueB] &&
+      game[valueB] === game[valueC] &&
+      game[valueA] !== null
 
     if (isWinner) {
       return game[valueA] // return winner value X or O
